5756327e7f2af3b841aa3efec4497aa8bfe268ed
Mekyle Naidoo <mekylenaidoo@yahoo.com>
2018-06-17 22:33:00 +0200

    Wallet updates

--- Wallet/walletcoreEHDW.js
+++ Wallet/walletcoreEHDW.js
@@ -1,3 +1,20 @@
 const { generateMnemonic, EthHdWallet } = require('eth-hd-wallet')
 
-const wallet = EthHdWallet.fromMnemonic(generateMnemonic());
+const wallet = EthHdWallet.fromMnemonic(generateMnemonic())
+
+console.log( wallet instanceof EthHdWallet );
+console.log( wallet.generateAddresses(2) )
+
+const rawTx = wallet.sign({
+  from: '0x...',
+  to: '0x...',
+  value: 200000000000000000,
+  nonce: 0x0,
+  gasPrice: 50000000000,
+  gasLimit: 21000,
+  chainId: 1 /* see https://github.com/ethereum/EIPs/blob/master/EIPS/eip-155.md */
+})
+
+console.log( rawTx ) /* "0x...." */
+
+web3.eth.sendRawTransaction(rawTx, (err) => { ... })
--- Wallet/walletcorescratch.js
+++ Wallet/walletcorescratch.js
@@ -11,3 +11,17 @@ const root = hdkey.fromMasterSeed(seed);
 const masterPrivateKey = root.privateKey.toString('Hex');// This creates the master private key
 const addrNode = root.derivePath("m/44'/60'/0'/0/0");
 const address = addrNode.getWallet().getChecksumAddressString();
+
+const rawTx = wallet.sign({
+  from: '0x...',
+  to: '0x...',
+  value: 200000000000000000,
+  nonce: 0x0,
+  gasPrice: 50000000000,
+  gasLimit: 21000,
+  chainId: 1 /* see https://github.com/ethereum/EIPs/blob/master/EIPS/eip-155.md */
+})
+
+console.log( rawTx ) /* "0x...." */
+
+web3.eth.sendRawTransaction(rawTx, (err) => { ... })
